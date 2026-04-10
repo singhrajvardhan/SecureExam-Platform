a84b1dab71f18a2d069a80678318e44bd2e4c094
Devansh Gupta <work.devansh14@gmail.com>
2026-04-10 17:52:15 +0000

    update new feature 3

--- teacher/login.php
+++ teacher/login.php
@@ -1,8 +1,6 @@
 <?php
 require_once '../config.php';
-
 session_start();
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = md5($_POST['password']);
